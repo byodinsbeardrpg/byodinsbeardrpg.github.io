0a32b1384a4a0bee65496590449980b3acd5d862
Colin Le Sueur <colin@byodinsbeardrpg.com>
2021-09-02 21:21:38 +0100

    Create my-jekyll-plugin-code.rb

---
+++ _plugins/my-jekyll-plugin-code.rb
@@ -0,0 +1,9 @@
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  if doc.output_ext == ".html"
+    doc.output =
+      doc.output.gsub(
+        /<h([1-6])(.*?)id="([\w-]+)"(.*?)>(.*?)<\/h[1-6]>/,
+        '<a href="#\3"><h\1\2id="\3"\4>\5</h\1></a>'
+      )
+  end
+end
