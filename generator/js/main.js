5c362e3ef2fbe23f6b05b22bd1b607e63d758ea1
Colin Le Sueur <colin@byodinsbeardrpg.com>
2021-09-03 15:06:36 +0100

    Update main.js

--- generator/js/main.js
+++ generator/js/main.js
@@ -1,43 +1,6 @@
 roll = (d) => {
   return Math.floor(Math.random()*d)+1;
 };
-const armor = generate_text("armor");
-const helmet = generate_text("helmet");
-const tool = generate_text("tool");
-const gear = generate_text("gear");
-const trinket = generate_text("trinket");
-const weapons = generate_text("weapons");
-const bonus = generate_text("bonus");
-const res = generate_text("res");
-
-let allItems = [];
-allItems.push(armor, helmet, weapons, gear, tool, trinket, bonus);
-let total = 2;
-for (let i = 0; i < allItems.length; i++) {
-  let item = allItems[i];
-  if (item.includes("bulky")) {
-    total += 2;
-  } else if (total > 10){
-    total = 10;
-  } else if (!item.includes("protection") && (!item.includes("nor") && (!item.includes("stacks")))) {
-    total++;
-  }
-}
-
-let armorTotal = 0;
-let armorValue = [];
-armorValue.push(armor, helmet);
-armorValue.forEach((item) => {
-  if (item.includes("1")) {
-    armorTotal++;
-  } if (armorTotal > 3){
-      armorTotal = 3;
-  } else if (item.includes("2")) {
-    armorTotal += 2;
-  } else if (item.includes("3")) {
-    armorTotal += 3;
-  }
-});
 
 console.log(total);
 $("#character").html(generate_text("character"));
@@ -48,9 +11,3 @@ $("#" + item).html(roll(6) + roll(6) + roll(6));});
 $("#armorTotal").html(armorTotal);
 $("#total").html(total);
 $("#gold").html(roll(6) + roll(6) + roll(6));
-function Resilience() {
-  var vig = document.getElementById("#vit").value;
-  var vit = document.getElementById("#vig").value;
-  var res = +vig + +vit;
-  document.getElementById("#res").innerHTML = res;
-}
