a39cc5218b512f887d80b9fb5f0157173439b2e8
Colin Le Sueur <colin@byodinsbeardrpg.com>
2021-09-03 15:49:09 +0100

    Update main.js

--- generator/js/main.js
+++ generator/js/main.js
@@ -5,7 +5,7 @@ roll = (d) => {
 $("#character").html(generate_text("character"));
 $("#vit").html(roll(6));
 $("#vig").html(roll(6));
-var res = ${"vit"} + ${"vig"};
+var res = '${"vit"} + ${"vig"}';
 console.log(res);
 ["str", "dex", "wit", "spi"].forEach((item, i) => {
 $("#" + item).html(roll(6) + roll(6) + roll(6));});
