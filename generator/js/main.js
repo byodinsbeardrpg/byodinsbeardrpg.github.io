8a12ed2b2e1d7dfef8367ab4f36b44b3b2148ca9
Colin Le Sueur <colin@byodinsbeardrpg.com>
2021-09-04 10:08:30 +0100

    Update main.js

--- generator/js/main.js
+++ generator/js/main.js
@@ -2,15 +2,6 @@ roll = (d) => {
   return Math.floor(Math.random()*d)+1;
 };
 $("#character").html(generate_text("character"));
-if (character.includes("warrior")){
-  defTotal = 3;
-} else if (character.includes("skald")) {
-  defTotal = 2;
-} else if (character.includes("scout")) {
-  defTotal = 1;
-} else if (character.includes("seer")) {
-  defTotal = 1;
-}
 $("#vit").html(roll(6));
 $("#vig").html(roll(6));
 var vit = $("#vit").text();
