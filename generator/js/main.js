fd26c64c7fb9a916364186cf93a9684562c23bde
Colin Le Sueur <colin@byodinsbeardrpg.com>
2021-09-04 08:56:42 +0100

    Update main.js

--- generator/js/main.js
+++ generator/js/main.js
@@ -1,7 +1,11 @@
 roll = (d) => {
   return Math.floor(Math.random()*d)+1;
 };
-
+const class = generate_text("class");
+const defence = generate_text("defence");
+const vit = generate_text("vit");
+const vig = generate_text("vig");
+const res = generate_text("res");
 $("#character").html(generate_text("character"));
 $("#vit").html(roll(6));
 $("#vig").html(roll(6));
